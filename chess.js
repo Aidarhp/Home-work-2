3c5e45a227446c851459e77f7bbbdafb535532e6
Aidarhp <112775850+Aidarhp@users.noreply.github.com>
2022-09-09 18:47:58 +0600

    Add files via upload

---
+++ chess.js
@@ -0,0 +1,13 @@
+let king = 0;
+let kinga = 1;
+
+    while (kinga < 10) {
+    console.log('# # # #');
+    if (kinga == 10) {
+        break;
+    }if (kinga) {
+        console.log('','* * * *');
+    }
+    kinga++;
+}
+
---
+++ main.js
@@ -0,0 +1,31 @@
+const triangle = [
+    {
+        key: '*'
+    },
+    {
+        key: "**"
+    },
+    {
+        key: "***"
+    },
+    {
+        key: "****"
+    },
+    {
+        key: "*****"
+    },
+    {
+        key: "******"
+    },
+    {
+        key: "*******"
+    },
+]
+
+console.log(triangle[0].key);
+console.log(triangle[1].key);
+console.log(triangle[2].key);
+console.log(triangle[3].key);
+console.log(triangle[4].key);
+console.log(triangle[5].key);
+console.log(triangle[6].key);
